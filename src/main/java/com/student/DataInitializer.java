81e26dde47fc10e4421a340708c9d1b55ba2913b
prashantpiyush1111 <Prashantpiyush1111@gmail.com>
2026-05-02 12:29:56 +0530

    Add DataInitializer to insert sample student records on application startup

---
+++ src/main/java/com/student/DataInitializer.java
@@ -0,0 +1,25 @@
+package com.student;
+
+import com.student.model.Student;
+import com.student.repository.StudentRepository;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class DataInitializer {
+
+    @Bean
+    CommandLineRunner initData(StudentRepository repository) {
+        return args -> {
+
+            repository.save(new Student(null, "Ram", "Ram@email.com", "Computer Science", 20, "03001234567"));
+            repository.save(new Student(null, "Radha", "Radha@email.com", "Software Engineering", 21, "03011234567"));
+            repository.save(new Student(null, "Krishna", "Krishna@email.com", "Computer Science", 22, "03021234567"));
+            repository.save(new Student(null, "Sita", "Sita@email.com", "Data Science", 19, "03031234567"));
+            repository.save(new Student(null, "Hanuman", "Hanuman@email.com", "Cyber Security", 23, "03041234567"));
+
+            System.out.println("Sample data added successfully!");
+        };
+    }
+}
