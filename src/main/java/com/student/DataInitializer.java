bbccd440eb81ac8d295c77dcb329547d351ecdb5
prashantpiyush1111 <Prashantpiyush1111@gmail.com>
2026-05-03 11:09:03 +0530

    feat: add Admin entity model for authentication system

--- src/main/java/com/student/DataInitializer.java
+++ src/main/java/com/student/DataInitializer.java
@@ -13,11 +13,11 @@ public class DataInitializer {
     CommandLineRunner initData(StudentRepository repository) {
         return args -> {
 
-            repository.save(new Student(null, "Ram", "Ram@email.com", "Computer Science", 20, "03001234567"));
-            repository.save(new Student(null, "Radha", "Radha@email.com", "Software Engineering", 21, "03011234567"));
-            repository.save(new Student(null, "Krishna", "Krishna@email.com", "Computer Science", 22, "03021234567"));
-            repository.save(new Student(null, "Sita", "Sita@email.com", "Data Science", 19, "03031234567"));
-            repository.save(new Student(null, "Hanuman", "Hanuman@email.com", "Cyber Security", 23, "03041234567"));
+            repository.save(new Student(null, "Ram", "Raam@email.com", "Computer Science", 20, "03001234567"));
+            repository.save(new Student(null, "Radha", "Raadha@email.com", "Software Engineering", 21, "03011234567"));
+            repository.save(new Student(null, "Krishna", "Krrishna@email.com", "Computer Science", 22, "03021234567"));
+            repository.save(new Student(null, "Sita", "Siita@email.com", "Data Science", 19, "03031234567"));
+            repository.save(new Student(null, "Hanuman", "Haanuman@email.com", "Cyber Security", 23, "03041234567"));
 
             System.out.println("Sample data added successfully!");
         };
