35d69e15d6a3e01d28a399e558bae4fa17f23b98
prashantpiyush1111 <Prashantpiyush1111@gmail.com>
2026-05-06 00:21:12 +0530

    chore: remove unused imports from StudentController

--- src/main/java/com/student/controller/DashboardController.java
+++ src/main/java/com/student/controller/DashboardController.java
@@ -1,6 +1,6 @@
 package com.student.controller;
 
-import jakarta.servlet.http.HttpSession;
+
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.stereotype.Controller;
